dcddfa0fdde443e2ee41aeaba1a8cd3c0b57aa88
Patrik Majer <patrik.majer.pisek@gmail.com>
2020-09-12 22:51:03 +0200

    hpa-hq1: add terraform providers

--- configs-servers/hpa-hq1/puppet-hpa-core.pp
+++ configs-servers/hpa-hq1/puppet-hpa-core.pp
@@ -59,3 +59,40 @@ archive { 'terraform_0.12.29_linux_amd64.zip':
   group         => 'root',
   require       => File[$terraform_install_dir],
 }
+
+$terraform_plugin_dir = ['/root/terraform.d',
+                                '/root/terraform.d/plugins',
+                                '/root/terraform.d/plugins/linux_amd64',
+]
+
+file { $terraform_plugin_dir:
+  ensure  => directory,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0755',
+  require => Vcsrepo['/root/h-platform-automation-core'],
+}
+
+archive { 'terraform-provider-ct_0.6.1_linux_amd64.zip':
+  path          => '/tmp/terraform-provider-ct_0.6.1_linux_amd64.zip',
+  source        => 'https://github.com/poseidon/terraform-provider-ct/releases/download/v0.6.1/terraform-provider-ct_0.6.1_linux_amd64.zip',
+  extract       => true,
+  extract_path  => '/root/terraform.d/plugins/linux_amd64',
+  creates       => '/root/terraform.d/plugins/linux_amd64/terraform-provider-ct_v0.6.1',
+  cleanup       => true,
+  user          => 'root',
+  group         => 'root',
+  require       => File[$terraform_plugin_dir],
+}
+
+archive { 'terraform-provider-libvirt-0.6.2+git.1585292411.8cbe9ad0.Ubuntu_18.04.amd64.tar.gz':
+  path          => '/tmp/terraform-provider-libvirt-0.6.2+git.1585292411.8cbe9ad0.Ubuntu_18.04.amd64.tar.gz',
+  source        => 'https://github.com/dmacvicar/terraform-provider-libvirt/releases/download/v0.6.2/terraform-provider-libvirt-0.6.2+git.1585292411.8cbe9ad0.Ubuntu_18.04.amd64.tar.gz',
+  extract       => true,
+  extract_path  => '/root/terraform.d/plugins/linux_amd64',
+  creates       => '/root/terraform.d/plugins/linux_amd64/terraform-provider-libvirt',
+  cleanup       => true,
+  user          => 'root',
+  group         => 'root',
+  require       => File[$terraform_plugin_dir],
+}
